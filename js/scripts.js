9d9aaeef4f70914d4ba888c405e70b38faf2cba2
Andrew Williams <andrew.williams@enterpriseautomation.co.uk>
2019-03-10 12:22:00 +0000

    added navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
+let last_known_scroll_position = 0;
+let ticking = false;
+
 function hideSplashContainer() {
   var splashContainer = document.querySelector('.splashContainer');
-  splashContainer.classList.add('hide')
-}
+  splashContainer.classList.add('hide');
+};
+
+function doSomething(scroll_pos) {
+  var navigation = document.querySelector('.navigation');
+  if (scroll_pos >= 20) {
+    navigation.classList.add('alt');
+  } else {
+    navigation.classList.remove('alt');
+  };
+};
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
